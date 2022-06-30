d48406ea5a02d53735c278b1fd8d05b3221f5c70
azharjk <azhr.project@gmail.com>
2022-06-30 12:08:55 +0700

    Add user interface page

---
+++ app.js
@@ -0,0 +1,43 @@
+const renderChart = (selector, type, data) => {
+  const barChartCanvas = $(selector)[0];
+  new Chart(barChartCanvas, {
+    type,
+    data: {
+      labels: data.labels,
+      datasets: [{
+        label: 'Penilaian',
+        backgroundColor: 'rgb(255, 99, 132)',
+        data: data.penilaian,
+        backgroundColor: data.colors
+      }]
+    }
+  });
+}
+
+$(document).ready(function () {
+  $('#data-table').DataTable({
+    fnInitComplete: function (settings, { data }) {
+      let labels = [];
+      let penilaian = [];
+      let colors = [];
+
+      data.forEach((v) => {
+        labels.push(v.nama);
+        penilaian.push(v.penilaian);
+        colors.push(v.color);
+      });
+
+      renderChart('#bar-chart', 'bar', { labels, penilaian, colors });
+      renderChart('#line-chart', 'line', { labels, penilaian, colors });
+    },
+    ajax: {
+      url: '/api_json.php',
+      dataSrc: 'data'
+    },
+    columns: [
+      { data: 'nama' },
+      { data: 'jabatan' },
+      { data: 'penilaian' }
+    ]
+  });
+});
