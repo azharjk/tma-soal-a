30d6835fd9f63c2a080c7c009286d0277002e394
azharjk <azhr.project@gmail.com>
2022-06-30 12:07:35 +0700

    Expose color on API json

--- query.php
+++ query.php
@@ -2,7 +2,7 @@
 
 function get_result_query($conn): array
 {
-  $sql = file_get_contents('klasifikasi_penilaian.sql');
+  $sql = file_get_contents('sql_script/klasifikasi_penilaian.sql');
 
   $result = $conn->query($sql);
   if (!$result) {
@@ -18,29 +18,34 @@ function get_result_query($conn): array
     $penilaian = $value[3];
     $klasifikasi_penilaian = $value[4];
 
+    $html = '';
+    $color = '';
+
+    switch ($klasifikasi_penilaian) {
+      case 'B':
+        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: lightblue;">' . $klasifikasi_penilaian . '</p>';;
+        $color = 'lightblue';
+        break;
+      case 'A':
+        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: blue;">' . $klasifikasi_penilaian . '</p>';
+        $color = 'blue';
+        break;
+      case 'A+':
+        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: green;">' . $klasifikasi_penilaian . '</p>';
+        $color = 'green';
+        break;
+      default:
+        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: ' . $klasifikasi_penilaian . '</p>';
+    }
+
     $json = [
       'id' => $id,
       'nama' => $nama,
       'jabatan' => $jabatan,
       'penilaian' => $penilaian,
       'klasifikasi_penilaian' => $klasifikasi_penilaian,
+      'color' => $color
     ];
-
-    $html = '';
-
-    switch ($klasifikasi_penilaian) {
-      case 'B':
-        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: lightblue;">' . $klasifikasi_penilaian . '</p>';;
-        break;
-      case 'A':
-        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: blue;">' . $klasifikasi_penilaian . '</p>';
-        break;
-      case 'A+':
-        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: <span style="color: green;">' . $klasifikasi_penilaian . '</p>';
-        break;
-      default:
-        $html = '<p>Nama: ' . $nama . ', Jabatan: ' . $jabatan  . ', Penilaian: ' . $penilaian . ', Klasifikasi Penilaian: ' . $klasifikasi_penilaian . '</p>';
-    }
 
     array_push($ret, [
       'html' => $html,
