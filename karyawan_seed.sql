28006da9fbce9d58e0e8fdb954b4fa5f6f186590
azharjk <azhr.project@gmail.com>
2022-06-30 09:37:06 +0700

    Add table karyawan seed script

---
+++ karyawan_seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO karyawan (nama, jabatan, penilaian) VALUES
+("Alice", "HRD", 50),
+("Jordan", "Bos", 60),
+("Smith", "HRD", 70),
+("Fred", "Basic", 80),
+("Jack", "Basic", 90),
+("Joy", "Basic", 91),
+("Jill", "Basic", 95),
+("John Doe", "CEO", 100);
